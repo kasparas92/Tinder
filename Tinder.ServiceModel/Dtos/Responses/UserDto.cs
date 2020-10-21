a4ceb5396bf87d588c45ec343468f91116386ce0
VILNIUS\kasparas.jurkevicius <kasparas.jurkevicius@euromonitor.com>
2020-10-21 15:31:38 +0300

    Changes

--- Tinder.ServiceModel/Dtos/Responses/UserDto.cs
+++ Tinder.ServiceModel/Dtos/Responses/UserDto.cs
@@ -7,9 +7,14 @@ namespace Tinder.ServiceModel.Dtos.Responses
     {
         public string Name { get; set; }
         public int Age { get; set; }
+        public string KnowAs { get; set; }
         public string PhotoUrl { get; set; }
         public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
         public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
         public string Country { get; set; }
         public ICollection<PhotoDto> Photos { get; set; }
     }
